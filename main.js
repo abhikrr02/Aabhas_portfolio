9387b8aaf23d0d42072913d4716e1c0c79b3fcca
Aabhas Kumar <aabhaskumar@Aabhass-MacBook-Air.local>
2025-02-22 20:03:11 +0530

    active tabs

--- main.js
+++ main.js
@@ -7,4 +7,34 @@ document.addEventListener('DOMContentLoaded', () => {
     );
   });
 
+window.onload = () => {
+  const navLinks = document.querySelectorAll('.nav-btn');
+  const navLinksMobile = document.querySelectorAll('.nav-btn-mobile');
+  // console.log(navLinks);
+  
+  // navLinks.forEach(navLink => {
+  //   navLink.addEventListener('click', (event) =>{
+  //     // console.log(event);
+  //     // document.querySelector('.active').classList.remove('active');
+  //     navLink.classList.add('active');
+  //   })
+  // })
 
+  let myPageTag = window.location.href;
+  console.log(myPageTag);
+  if (myPageTag.includes('index')) {
+    navLinks[0].classList.add('active');
+    navLinksMobile[0].classList.add('active');
+  } else if(myPageTag.includes('resume')) {
+    navLinks[1].classList.add('active');
+    navLinksMobile[1].classList.add('active');
+  } else if (myPageTag.includes('projects')) {
+    navLinks[2].classList.add('active');
+    navLinksMobile[2].classList.add('active');
+  } else if (myPageTag.includes('contact-me')) {
+    navLinks[3].classList.add('active');
+    navLinksMobile[3].classList.add('active');
+  }
+}
+
+  
